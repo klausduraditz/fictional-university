ea95ca9198b370094bbda03ae943c18ab9cd1a15
Franz Rosenberger <franzrosenberger@coronado.local>
2018-10-19 14:56:54 +0200

    section 9: programs post type

--- themes/wp-training-project/functions.php
+++ themes/wp-training-project/functions.php
@@ -24,10 +24,17 @@
     // invoke university_features after page is loaded and theme is initialized
     add_action('after_setup_theme', 'university_features');
 
-
+    // Adjust default WP Queries if only subtle changes are needed
+    // -> avoid creating a custom query if only little changes to default query are needed
     function university_adjust_queries($query) {
+        if (!is_admin() && is_post_type_archive('program') && $query->is_main_query()) {
+            $query->set('orderby', 'title');
+            $query->set('order', 'ASC');
+            $query->set('posts_per_page', -1);
+        }
+
         // Apply only if not Backend AND only on Archive-Pages AND query is main WP URL Query (to not affect custom queries)
-        if (!is_admin() && is_post_type_archive() && $query->is_main_query()) {
+        if (!is_admin() && is_post_type_archive('event') && $query->is_main_query()) {
             $today = date('Ymd');
             $query->set('meta_key', 'event_date');
             $query->set('orderby', 'meta_value_num');
--- themes/wp-training-project/single-event.php
+++ themes/wp-training-project/single-event.php
@@ -26,8 +26,23 @@
                     </span>
                 </p>
             </div>
-             
+
             <div class="generic-content"><?php the_content(); ?></div>
+
+            <?php
+                // get_field Function from ACF Plugin to access custom fields
+                $relatedPrograms = get_field('related_programs');
+                
+                if ($relatedPrograms) { ?>
+                    <hr class="section-break">
+                    <h2 class="headline headline--medium">Related Program(s)</h2>
+                    <ul class="link-list min-list">
+                    <?php 
+                        foreach($relatedPrograms as $program) { ?>
+                            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+                    <?php } ?>
+                <?php } ?>
+            </ul>
         </div>
 
     <?php }
